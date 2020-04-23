7690a88dfc66c378e6b021ca7f44445910a2cc32
Adrian Garmendia <dejima@gmail.com>
2020-04-23 13:54:03 -0500

    part 3

--- scripts/journal.js
+++ scripts/journal.js
@@ -1,38 +1,40 @@
-/*
-    Define the keys and value for a JavaScript object that
-    represents a journal entry about what you learned today
-*/
-const journalEntry1 = {
-	journalEntryDate: 04 / 17 / 2020,
-	conceptsCoveredEntry: "JavaScript syntax",
-	textAreaEntry:
-		"Im the worst at syntax, need to get syntax for dummies or something",
-	moodDayEntry: "OK",
-};
-const journalEntry2 = {
-	journalEntryDate: 04 / 18 / 2020,
-	conceptsCoveredEntry: "CSS for dummies",
-	textAreaEntry: "Simple and clean, dont get crazy Adrian",
-	moodDayEntry: "Happy",
-};
-const journalEntry3 = {
-	journalEntryDate: 04 / 19 / 2020,
-	conceptsCoveredEntry: "Daily Journal part 2",
-	textAreaEntry: "Ask instructor if this is right, doesnt look right to me.",
-	moodDayEntry: "Sad",
-};
+const journalEntries = [
+	{
+		date: "04 / 17 / 2020",
+		concept: "JavaScript syntax",
+		entry:
+			"Im the worst at syntax, need to get syntax for dummies or something",
+		mood: "OK",
+	},
+	{
+		date: "04 / 18 / 2020",
+		concept: "CSS for dummies",
+		entry: "Simple and clean, dont get crazy Adrian",
+		mood: "Happy",
+	},
+	{
+		date: "04 / 19 / 2020",
+		concept: "Daily Journal part 2",
+		entry: "Ask instructor if this is right, doesnt look right to me.",
+		mood: "Sad",
+	},
+];
 
-// Eventually, you will have multiple journal entries, so you need to define an appropriately named variable that will have the value of an array.
-
-// Once you define that variable and give it a default value of a blank array, use the push() method to add the journalEntry object you defined above to the array.
-
-const journalEntryCollection = [];
-
-const pushEntry = function (entry) {
-	journalEntryCollection.push(entry);
+const makeJournalEntryComponent = (journalEntry) => {
+	return `
+	<div>
+			<h1>${journalEntry.concept}</h1>
+			<p>${journalEntry.entry}<br>${journalEntry.date}</p>
+	</div>
+	`;
 };
 
-pushEntry(journalEntry1);
-pushEntry(journalEntry2);
-pushEntry(journalEntry3);
-console.log(journalEntryCollection);
+const renderJournalEntries = (entries) => {
+	const entryContainer = document.querySelector(".entryLog");
+	for (let i = 0; i < entries.length; i++) {
+		const entry = entries[i];
+		entryContainer.innerHTML += makeJournalEntryComponent(entry);
+	}
+};
+
+renderJournalEntries(journalEntries);
