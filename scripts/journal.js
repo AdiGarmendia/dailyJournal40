71b959ad14f8ccbd4f50b7c14f1701f2f0fe1d96
Adrian Garmendia <dejima@gmail.com>
2020-04-23 15:02:42 -0500

    part 4

--- scripts/journal.js
+++ scripts/journal.js
@@ -1,26 +1,4 @@
-const journalEntries = [
-	{
-		date: "04 / 17 / 2020",
-		concept: "JavaScript syntax",
-		entry:
-			"Im the worst at syntax, need to get syntax for dummies or something",
-		mood: "OK",
-	},
-	{
-		date: "04 / 18 / 2020",
-		concept: "CSS for dummies",
-		entry: "Simple and clean, dont get crazy Adrian",
-		mood: "Happy",
-	},
-	{
-		date: "04 / 19 / 2020",
-		concept: "Daily Journal part 2",
-		entry: "Ask instructor if this is right, doesnt look right to me.",
-		mood: "Sad",
-	},
-];
-
-const makeJournalEntryComponent = (journalEntry) => {
+const createEntryComponent = (journalEntry) => {
 	return `
 	<div>
 			<h1>${journalEntry.concept}</h1>
@@ -29,12 +7,13 @@ const makeJournalEntryComponent = (journalEntry) => {
 	`;
 };
 
-const renderJournalEntries = (entries) => {
-	const entryContainer = document.querySelector(".entryLog");
-	for (let i = 0; i < entries.length; i++) {
-		const entry = entries[i];
-		entryContainer.innerHTML += makeJournalEntryComponent(entry);
-	}
-};
+const entryContainer = document.querySelector(".entryLog");
 
-renderJournalEntries(journalEntries);
+fetch("http://localhost:8088/entries")
+	.then((entries) => entries.json())
+	.then((parsedEntries) => {
+		parsedEntries.forEach((entry) => {
+			entryComponent = createEntryComponent(entry);
+			entryContainer.innerHTML += entryComponent;
+		});
+	});
